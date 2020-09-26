4370fb59080dedb5ea8e37323e4bbbf5600e46a1
juliocsantaman <juliocsantaman@gmail.com>
2020-09-26 12:27:14 -0500

    Arreglando el código.

--- src/main/java/Algoritmos.java
+++ src/main/java/Algoritmos.java
@@ -17,31 +17,44 @@ public class Algoritmos {
     
     public int[] calcularNumPrimos(int n) {
         int[] numPrimos = new int[n];
-        int cantidad = 0;
-        int numero = 1;
-        int cantDivisores = 0;
-        int indice = 0;
+        int cantidad = 0, numero = 1, cantDivisores = 0, indice = 0;
         
         // Algoritmo.
+        // La cantidad debe ser igual a n.
         while(cantidad < 5) {
-           for(int j=1; j<=numero; j++) {
-               if(numero % j == 0) {
+            /*
+            Estamos calculando la cantidad de divisores
+            que tiene la variable numero.
+            */
+           for(int i=1; i<=numero; i++) {
+               if(numero % i == 0) {
                   cantDivisores++; 
                }
            }
            
+           /*
+           Sabemos que un número primo tiene 
+           dos divisores, el 1 y él mismo. Entonces
+           comprobamos si el valor de la variable numero
+           es un número primo con ayuda de la variable
+           cantDivisores.
+           */
            if(cantDivisores == 2) {
-               numPrimos[indice] = numero;
+               numPrimos[indice] = numero; // Asignamos el número primo al arreglo.
                cantidad++;
                indice++;
            }
            
+           // Reiniciamos cantDivisores para volver a comprobar.
            cantDivisores = 0;
+           // Aumentamos en uno para seguir comparando los demás números.
            numero++;
         }
         
         return numPrimos;
     }
+    
+    
     
     public void imprimirArreglo(int[] arreglo) {
         for(int i=0; i<arreglo.length; i++) {
